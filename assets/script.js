a04e5b595adf456192cfc225498cb741d4d0dad3
Ian Darland <ianpdarland@gmail.com>
2021-09-18 23:27:17 -0500

    generating cards for search results

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,71 @@
 // const axios = require('axios');
 
+let searchTerm = {
+    keyword: '',
+    author: ""
+}
+
 let searchResults = []
 
 $(root).append(`<p>howdy yall</p>`)
 
-$(".search-bar").change(
+$('.author-search').keyup(
     function (){
-        const searchTerm = this.value
+        const authorEnter = this.value.split(' ').join('+').toLowerCase()
+        console.log(authorEnter)
+        searchTerm = {...searchTerm, author: authorEnter}
         console.log(searchTerm)
+    }
+    )
+    
+    $(".search-bar").keyup(
+        function (){
+            const keywordEnter = this.value.split(' ').join('+').toLowerCase()
+            searchTerm = {...searchTerm, keyword: keywordEnter}
+            console.log(searchTerm)
+    }
+    )
+    
+    $('.run-search').click(
+        function (event){
+        event.preventDefault()
         $(root).append(`<p>${searchTerm}</p>`)
+        $(root).empty()
         searchBooks(searchTerm)
         console.log(searchResults)
+
     }
 )
 
 const searchBooks = (data) =>{
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${data}&key=AIzaSyB8Jvy0dfcWAybRKZyYUBicgxyetrhSaLw`)
+    let author = ''
+    if(data.author){
+        author = `+inauthor:${data.author}`
+    }
+    if(!data.keyword){
+        data.keyword = data.author
+    }
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${data.keyword}${author}&key=AIzaSyB8Jvy0dfcWAybRKZyYUBicgxyetrhSaLw`)
         .then(res => res.json())
         .then(res => {
             res.items.map(item =>{
-                $(root).append(`<p>${item.volumeInfo.title} by: ${item.volumeInfo.authors[0]}</p>`)
+                $(root).append(
+                    `<div class="card col-12 col-md-6 col-lg-3" style="width: 18rem;">
+                        <img src="${item.volumeInfo.imageLinks.thumbnail}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${item.volumeInfo.title}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">${item.volumeInfo.authors[0]}</h6>
+                            <p class="card-text card-desc" maxlength ="200">${item.volumeInfo.description}</p>
+                        <a href="${item.volumeInfo.infoLink}" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>`)
             })
             console.log(res.items)
             searchResults = res.items})
+        .then(
+            searchTerm = {
+                keyword: $('.search-bar').val(),
+                author: $('.author-search').val()
+            }
+        )
 }
