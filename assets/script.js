3821a5df2726aadb473b9e17624ea4e48f1f59b4
Ian Darland <ianpdarland@gmail.com>
2021-09-20 13:28:14 -0500

    description collapsed and toggling

--- assets/script.js
+++ assets/script.js
@@ -48,18 +48,31 @@ const searchBooks = (data) =>{
     fetch(`https://www.googleapis.com/books/v1/volumes?q=${data.keyword}${author}&key=AIzaSyB8Jvy0dfcWAybRKZyYUBicgxyetrhSaLw`)
         .then(res => res.json())
         .then(res => {
-            res.items.map(item =>{
+            res.items.map((item, index) =>{
                 $(root).append(
                     `<div class="card col-12 col-md-6 col-lg-3" style="width: 18rem;">
                         <img src="${item.volumeInfo.imageLinks.thumbnail}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">${item.volumeInfo.title}</h5>
                             <h6 class="card-subtitle mb-2 text-muted">${item.volumeInfo.authors[0]}</h6>
-                            <p class="card-text card-desc" maxlength ="200">${item.volumeInfo.description}</p>
-                        <a href="${item.volumeInfo.infoLink}" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>`)
-            })
+                            <p>
+                            <a class="btn btn-primary" href="${item.volumeInfo.infoLink}" role="button">
+                                Purchase
+                            </a>
+                            <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample${index}" aria-expanded="false" aria-controls="collapseExample">
+                                Description
+                            </button>
+                            </p>
+                            <div class="collapse" id="collapseExample${index}">
+                                <div class="card card-body">
+                                ${item.volumeInfo.description}
+                                </div>
+                            </div>
+                            </div>
+                            </div>`)
+                        })
+                        //     <p class="card-text card-desc" maxlength ="200">${item.volumeInfo.description}</p>
+                        // <a href="${item.volumeInfo.infoLink}" class="btn btn-primary">Go somewhere</a>
             console.log(res.items)
             searchResults = res.items})
         .then(
