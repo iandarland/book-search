77eb8db83a5aec8618bc107b8899c47530229c71
Ian Darland <ianpdarland@gmail.com>
2021-10-25 20:13:00 -0500

    reading list rendering as expected

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,6 @@ $('.author-search').keyup(
         const authorEnter = this.value.split(' ').join('+').toLowerCase()
         console.log(authorEnter)
         searchTerm = {...searchTerm, author: authorEnter}
-        console.log(searchTerm)
     }
     )
     
@@ -25,7 +24,6 @@ $('.author-search').keyup(
         function (){
             const keywordEnter = this.value.split(' ').join('+').toLowerCase()
             searchTerm = {...searchTerm, keyword: keywordEnter}
-            console.log(searchTerm)
     }
     )
 
@@ -54,6 +52,15 @@ $('.author-search').keyup(
         // }
     }}
     )
+
+    $(document).on('click', '.remove-from-list',
+        function(event){
+            event.preventDefault()
+            console.log(this.value)
+            readingList = readingList.filter(e => e !== this.value)
+            renderReadingList(readingList)
+            localStorage.setItem("readingList", JSON.stringify(filteredAry))
+        })
     
     $('.run-search').click(
         function (event){
@@ -68,6 +75,13 @@ $('.author-search').keyup(
 
     }
 )
+    $('.reading-list').click(
+        async function(event){
+            event.preventDefault()
+            $('.reading-list').blur()
+            renderReadingList(readingList)
+        }
+    )
 
 const searchBooks = (data) =>{
     let author = ''
@@ -103,9 +117,9 @@ const searchBooks = (data) =>{
                                 ${item.volumeInfo.description}
                                 </div>
                             </div>
-                            </div>
-                            </div>`)
-                        })
+                        </div>
+                    </div>`)
+            })
                         //     <p class="card-text card-desc" maxlength ="200">${item.volumeInfo.description}</p>
                         // <a href="${item.volumeInfo.infoLink}" class="btn btn-primary">Go somewhere</a>
             console.log(res.items)
@@ -117,3 +131,44 @@ const searchBooks = (data) =>{
             }
         )
 }
+
+const getReadingList = async (data) =>{
+    let pulledList = await Promise.all(data.map(item => {
+        return fetch(`https://www.googleapis.com/books/v1/volumes/${item}`)
+            .then(res => res.json())
+    }))
+    return pulledList
+}
+
+const renderReadingList = async (data) =>{
+    console.log(data)
+    $(root).empty()
+    const fullList = await getReadingList(data)
+    fullList.map((item,index) => {
+        $(root).append(
+            `<div class="card col-12 col-md-6 col-lg-3" style="width: 18rem;">
+                <img src="${item.volumeInfo.imageLinks.thumbnail}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${item.volumeInfo.title}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${item.volumeInfo.authors[0]}</h6>
+                    <p>
+                        <button class="btn btn-primary remove-from-list" type="button" role="button" value="${item.id}">
+                            Remove
+                        </button>
+                        <a class="btn btn-primary" href="${item.volumeInfo.infoLink}" role="button">
+                            Purchase
+                        </a>
+                        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample${index}" aria-expanded="false" aria-controls="collapseExample">
+                            Description
+                        </button>
+                    </p>
+                    <div class="collapse" id="collapseExample${index}">
+                        <div class="card card-body">
+                            ${item.volumeInfo.description}
+                        </div>
+                    </div>
+                </div>
+            </div>`)
+    })
+}
+
